822d4ad71f3091bb773d9986a856056c1fa5d76d
ericsonga <ericson@cc.gatech.edu>
2017-10-21 10:15:09 -0400

    Updated the pavement.py file
    Updated

--- pavement.py
+++ pavement.py
@@ -2,46 +2,58 @@ import paver
 from paver.easy import *
 import paver.setuputils
 paver.setuputils.install_distutils_tasks()
+from socket import gethostname
 import os, sys
+from os import environ
 
 from sphinxcontrib import paverutils
 
 sys.path.append(os.getcwd())
 
 home_dir = os.getcwd()
-from socket import gethostname
+
 
 master_url = None
 if master_url is None:
-    if gethostname() == 'web407.webfaction.com':
+    if 'RSHOST' in os.environ:
+        master_url = environ['RSHOST']
+    elif gethostname() in  ['web608.webfaction.com', 'rsbuilder']:
         master_url = 'http://interactivepython.org'
+    elif gethostname() == 'runestone-deploy':
+        master_url = 'https://runestone.academy'
     else:
         master_url = 'http://127.0.0.1:8000'
         
 master_app = 'runestone'
-serving_dir = "./build/ex3ParsonsAdapt"
+serving_dir = "./build/Ex3ParsonsAdapt"
 dest = '../../static'
+project_name = "Ex3ParsonsAdapt"
 
 options(
     sphinx = Bunch(docroot=".",),
 
     build = Bunch(
-        builddir="./build/ex3ParsonsAdapt",
+        builddir="./build/Ex3ParsonsAdapt",
         sourcedir="_sources",
-        outdir="./build/ex3ParsonsAdapt",
+        outdir="./build/Ex3ParsonsAdapt",
         confdir=".",
         project_name = "Ex3",
-        template_args={'course_id': 'ex3ParsonsAdapt',
+        template_args={'course_id': 'Ex3ParsonsAdapt',
                        'login_required':'true',
                        'appname':master_app,
                        'loglevel': 10,
                        'course_url':master_url,
                        'use_services': 'true',
                        'python3': 'true',
-                       'basecourse': 'ex3ParsonsAdapt'
+                       'dburl': 'postgresql://runestone@localhost/runestone',
+                       'basecourse': 'Ex3ParsonsAdapt'
                         }
     )
 )
 
-from runestone import build  # build is called implicitly by the paver driver.
+# Check to see if we are building on our Jenkins build server, if so use the environment variables
+# to update the DB information for this build
+if 'DBHOST' in environ and  'DBPASS' in environ and 'DBUSER' in environ and 'DBNAME' in environ:
+    options.build.template_args['dburl'] = 'postgresql://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}'.format(**environ)
 
+from runestone import build  # build is called implicitly by the paver driver.
